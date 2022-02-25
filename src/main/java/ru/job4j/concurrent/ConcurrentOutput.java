f07fc2b5b68dbce0e1e502056667350647e29172
sergei159 <89082766060QAzQAz>
2022-02-25 12:23:08 +0500

    Состояние нити. [#229175]

--- src/main/java/ru/job4j/concurrent/ConcurrentOutput.java
+++ src/main/java/ru/job4j/concurrent/ConcurrentOutput.java
@@ -2,13 +2,22 @@ package ru.job4j.concurrent;
 
 public class ConcurrentOutput {
     public static void main(String[] args) {
-        int count = 10;
-        for (int i = 0; i < count ; i++) {
-             Thread thread = new Thread(
-                    () -> System.out.println(Thread.currentThread().getName())
-            );
-             thread.start();
+        Thread first = new Thread(
+                () -> System.out.println(Thread.currentThread().getName())
+        );
+        Thread second = new Thread(
+                () -> System.out.println(Thread.currentThread().getName())
+        );
+        first.start();
+        second.start();
+        while (first.getState() != Thread.State.TERMINATED
+        && second.getState() != Thread.State.TERMINATED ) {
+            System.out.println(first.getState() + " First\n");
+            System.out.println(second.getState() + " Second\n");
         }
-        System.out.println(Thread.currentThread().getName());
+        System.out.println(first.getState() + " First");
+        System.out.println(second.getState() + " Second");
+        System.out.println("Threads \"first\" and \"second\" are terminated");
     }
+
 }
