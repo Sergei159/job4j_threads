c859112e64c9764d654f4acd773815a41358df02
Sergei159 <qars2010@yandex.ru>
2022-03-01 00:09:36 +0500

    Скачивание файла с ограничением. [#144271 #270110]

--- src/main/java/ru/job4j/thread/Wget.java
+++ src/main/java/ru/job4j/thread/Wget.java
@@ -5,6 +5,15 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.net.URL;
 
+/**
+ * Программа должна скачивать файл из сети с ограничением по скорости скачки.
+ *
+ * Чтобы ограничить скорость скачивания, нужно засечь время скачивания 1024 байт.
+ * Если время меньше указанного, то нужно выставить паузу за счет Thread.sleep.
+ *
+ * Пауза должна вычисляться, а не быть константой.
+ */
+
 public class Wget implements Runnable {
     private final String url;
     private final String destination;
@@ -17,22 +26,34 @@ public class Wget implements Runnable {
         this.speed = speed;
     }
 
+    /**
+     * int downloadData - кол-во скаченных байт.
+     * как только downloadData достигает значение speed
+     * проверяется время от начала скачивания до этого условия.
+     * Если время меньше секунды, то вызываем sleep у потока на разницу между
+     * переменной time и 1 секундой.
+     * После паузы обнуляем downloadData
+     */
     @Override
     public void run() {
         try (BufferedInputStream in = new BufferedInputStream(new URL(url).openStream());
              FileOutputStream fileOutputStream = new FileOutputStream(destination)) {
             byte[] dataBuffer = new byte[1024];
             int bytesRead;
+            int downloadData = 0;
             long startTime = System.currentTimeMillis();
             while ((bytesRead = in.read(dataBuffer, 0, 1024)) != -1) {
                 fileOutputStream.write(dataBuffer, 0, bytesRead);
-                Thread.sleep(100);
-                long endTime = System.currentTimeMillis();
-                long time = endTime - startTime;
-                if (speed > 1024 / time) {
-                    Thread.sleep(speed - 1024 / time);
+                downloadData += bytesRead;
+                if (downloadData >= speed) {
+                    long endTime = System.currentTimeMillis();
+                    long time = endTime - startTime;
+                    if (time < 1000) {
+                        Thread.sleep(1000 - time);
+                        downloadData = 0;
+                        startTime = System.currentTimeMillis();
+                    }
                 }
-                startTime = System.currentTimeMillis();
             }
         } catch (IOException e) {
             e.printStackTrace();
@@ -42,6 +63,10 @@ public class Wget implements Runnable {
     }
 
     public static void main(String[] args) throws InterruptedException {
+        if (args.length != 3) {
+            throw new IllegalStateException("String[] args must contain three arguments: "
+                    + "url, name of target file, speed of downloading, b/s");
+        }
         String url = args[0];
         String destination = args[1];
         long speed = Integer.parseInt(args[2]);
