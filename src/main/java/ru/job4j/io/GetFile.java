5590e2072ca2e1dc624d14c422e0d1ed86bbc11a
sergei159 <89082766060QAzQAz>
2022-03-01 13:01:22 +0500

    Visibility. Общий ресурс вне критической секции [#1102]

---
+++ src/main/java/ru/job4j/io/GetFile.java
@@ -0,0 +1,38 @@
+package ru.job4j.io;
+
+import java.io.*;
+import java.util.function.Predicate;
+
+public final class GetFile {
+    private final File file;
+
+    public GetFile(File file) {
+        this.file = file;
+    }
+
+    public String getContent() throws IOException {
+        return getContentByPredicate(data -> true);
+    }
+
+    public String getContentWithoutUnicode() throws IOException {
+        return getContentByPredicate(data -> data < 0x80);
+    }
+
+    public String getContentByPredicate(Predicate<Character> filter) {
+        String output = " ";
+        try (BufferedReader in = new BufferedReader(
+                new FileReader(file))) {
+            output = "";
+            int data;
+            while ((data = in.read()) > 0) {
+                if (filter.test((char) data)) {
+                    output += (char) data;
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return output;
+    }
+
+}
---
+++ src/main/java/ru/job4j/io/SaveFile.java
@@ -0,0 +1,22 @@
+package ru.job4j.io;
+
+import java.io.*;
+
+public final class SaveFile {
+    private final File file;
+
+    public SaveFile(File file) {
+        this.file = file;
+    }
+
+    public void saveContent(String content) throws IOException {
+        try (BufferedWriter out = new BufferedWriter(
+                new FileWriter(file))) {
+            for (int index = 0; index < content.length(); index += 1) {
+                out.write(content.charAt(index));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
