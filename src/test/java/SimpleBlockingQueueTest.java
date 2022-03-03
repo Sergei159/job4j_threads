3bdb0471e2d9b72cacfccb82d718c33097fe883d
Sergei159 <qars2010@yandex.ru>
2022-03-04 00:07:01 +0500

    Junit тест для блокирующей очереди. [#68589]

--- src/test/java/SimpleBlockingQueueTest.java
+++ src/test/java/SimpleBlockingQueueTest.java
@@ -1,8 +1,11 @@
 import org.junit.Test;
 import ru.job4j.buffer.SimpleBlockingQueue;
 
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.stream.IntStream;
 
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
@@ -14,23 +17,17 @@ public class SimpleBlockingQueueTest {
         SimpleBlockingQueue<Integer> blockingQueue = new SimpleBlockingQueue<>(
                 new LinkedList<>(), 2
         );
-        Thread first = new Thread(
+        Thread first =  new Thread(
                 () -> {
-                    try {
-                        blockingQueue.offer(1);
-                    } catch (InterruptedException e) {
-                        Thread.currentThread().interrupt();
-                    }
-                    try {
-                        blockingQueue.offer(2);
-                    } catch (InterruptedException e) {
-                        Thread.currentThread().interrupt();
-                    }
-                    try {
-                        blockingQueue.offer(3);
-                    } catch (InterruptedException e) {
-                        Thread.currentThread().interrupt();
-                    }
+                    IntStream.range(0, 3).forEach(
+                            value -> {
+                                try {
+                                    blockingQueue.offer(value);
+                                }  catch (InterruptedException e) {
+                                    e.printStackTrace();
+                                }
+                            }
+                    );
                 }
         );
         Thread second = new Thread(
@@ -70,21 +67,15 @@ public class SimpleBlockingQueueTest {
         );
         Thread second = new Thread(
                 () -> {
-                    try {
-                        blockingQueue.offer(1);
-                    } catch (InterruptedException e) {
-                        Thread.currentThread().interrupt();
-                    }
-                    try {
-                        blockingQueue.offer(2);
-                    } catch (InterruptedException e) {
-                        Thread.currentThread().interrupt();
-                    }
-                    try {
-                        blockingQueue.offer(3);
-                    } catch (InterruptedException e) {
-                        Thread.currentThread().interrupt();
-                    }
+                    IntStream.range(0, 3).forEach(
+                            value -> {
+                                try {
+                                    blockingQueue.offer(value);
+                                }  catch (InterruptedException e) {
+                                    e.printStackTrace();
+                                }
+                            }
+                    );
                 }
         );
         first.start();
@@ -92,6 +83,43 @@ public class SimpleBlockingQueueTest {
         first.join();
         second.join();
         assertThat(blockingQueue.size(), is(2));
+    }
+
+    @Test
+    public void whenFetchAllThenGetIt() throws InterruptedException {
+        final CopyOnWriteArrayList<Integer> buffer = new CopyOnWriteArrayList<>();
+        final SimpleBlockingQueue<Integer> queue = new SimpleBlockingQueue<>();
+        Thread producer = new Thread(
+                () -> {
+                    IntStream.range(0, 5).forEach(
+                            value -> {
+                                try {
+                                    queue.offer(value);
+                                } catch (InterruptedException e) {
+                                    e.printStackTrace();
+                                }
+                            }
+                    );
+                }
+        );
+        producer.start();
+        Thread consumer = new Thread(
+                () -> {
+                    while (queue.size() != 0 || !Thread.currentThread().isInterrupted()) {
+                        try {
+                            buffer.add(queue.poll());
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                            Thread.currentThread().interrupt();
+                        }
+                    }
+                }
+        );
+        consumer.start();
+        producer.join();
+        consumer.interrupt();
+        consumer.join();
+        assertThat(buffer, is(Arrays.asList(0, 1, 2, 3, 4)));
     }
 
 }
