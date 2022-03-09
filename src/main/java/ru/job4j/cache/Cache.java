7adf016e23a7d75d6a460b89ccc564bd7faf44cd
sergei159 <89082766060QAzQAz>
2022-03-09 09:32:02 +0500

    Неблокирующий кеш [#4741 #271939]

--- src/main/java/ru/job4j/cache/Cache.java
+++ src/main/java/ru/job4j/cache/Cache.java
@@ -4,20 +4,36 @@ import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicBoolean;
 
+/**
+ * Класс имитирует неблокирующий кэш.
+ * метод update выкидывает исключение, если версия модели
+ * была параллельно изменена другим потоком.
+ */
 public class Cache {
     private final Map<Integer, Base> memory = new ConcurrentHashMap<>();
 
+    /**
+     * Атомарный метод вставки в кэш
+     */
     public boolean add(Base model) {
         return memory.putIfAbsent(model.getId(), model) == null;
     }
 
+
+    /**
+     * Метод обновляет данные в кэше.
+     * Если версии одной Base model уже отличаются, то выкидывается
+     * Optimistic exception
+     * @return new Base с обновленной версией.
+     */
     public boolean update(Base model) {
         return memory.computeIfPresent(model.getId(), (key, value) -> {
             if (value.getVersion() != model.getVersion()) {
                 throw new OptimisticException("Versions are not equal");
             }
-            value.setVersion(value.getVersion() + 1);
-            return model;
+            Base result =  new Base(model.getId(), model.getVersion() + 1);
+            result.setName(model.getName());
+            return result;
         }) != null;
     }
 
